e807c2747866888e2f7f3e2f0ecc91b8d6e590ae
Mats Hammarqvist <Mats@Matss-MacBook-Pro.local>
2017-01-31 23:00:21 +0100

    Initial Commit

---
+++ kortlekTests/kortlekTests.swift
@@ -0,0 +1,36 @@
+//
+//  kortlekTests.swift
+//  kortlekTests
+//
+//  Created by Mats Hammarqvist on 2017-01-31.
+//  Copyright © 2017 Mats Hammarqvist. All rights reserved.
+//
+
+import XCTest
+@testable import kortlek
+
+class kortlekTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
